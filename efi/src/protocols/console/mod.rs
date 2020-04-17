bb1b011804417a4b604c26b12285233948ce9094
Kiril Mihaylov <Kiril195@hotmail.com>
2020-04-18 00:11:05 +0300

    Added EfiSimpleTextInputProtocol

--- efi/src/protocols/console/mod.rs
+++ efi/src/protocols/console/mod.rs
@@ -1,1 +1,2 @@
+pub mod simple_text_input_protocol;
 pub mod simple_text_output_protocol;
---
+++ efi/src/protocols/console/simple_text_input_protocol.rs
@@ -0,0 +1,57 @@
+use crate::types::EfiEvent;
+use crate::guid::EfiGuid;
+use crate::status::{
+	EfiStatus,
+	EfiStatusEnum,
+};
+use crate::protocols::EfiProtocol;
+
+#[repr(C)]
+pub struct EfiSimpleTextInputProtocol {
+	reset: extern "efiapi" fn(*const Self, extended_verification: bool) -> EfiStatus,
+	read_key_stroke: extern "efiapi" fn(*const Self, key: *mut EfiInputKey) -> EfiStatus,
+	wait_for_key: EfiEvent,
+}
+
+impl EfiSimpleTextInputProtocol {
+	pub fn reset(&self, extended_verification: bool) -> EfiStatusEnum {
+		(self.reset)(
+			self,
+			extended_verification
+		).into_enum()
+	}
+
+	pub fn read_key_stroke(&self) -> EfiStatusEnum<EfiInputKey> {
+		let mut key: EfiInputKey = EfiInputKey {
+			scan_code: 0,
+			unicode_char: 0,
+		};
+		
+		(self.read_key_stroke)(
+			self,
+			&mut key
+		).into_enum_data(key)
+	}
+
+	pub fn wait_for_key(&self) -> EfiEvent {
+		self.wait_for_key
+	}
+
+	pub fn wait_for_key_mut(&mut self) -> &mut EfiEvent {
+		&mut self.wait_for_key
+	}
+}
+
+impl EfiProtocol for EfiSimpleTextInputProtocol {
+	type Interface = Self;
+
+	fn guid() -> EfiGuid {
+		EfiGuid::from_tuple((0x387477c1, 0x69c7, 0x11d2, [0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b]))
+	}
+}
+
+#[repr(C)]
+pub struct EfiInputKey {
+	scan_code: u16,
+	unicode_char: u16,
+}
