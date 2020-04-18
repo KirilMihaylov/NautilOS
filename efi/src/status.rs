bbf91d22773bf77e3491348efc314fd24666b376
Kiril Mihaylov <Kiril195@hotmail.com>
2020-04-18 14:41:20 +0300

    Added EfiStatusWarning and EfiStatusError enums

--- efi/src/status.rs
+++ efi/src/status.rs
@@ -65,6 +65,72 @@ impl EfiStatus {
 			Error(self.0, error_data)
 		}
 	}
+
+	pub fn get_warning(&self) -> EfiStatusWarning {
+		use EfiStatusWarning::*;
+
+		if self.is_warning() {
+			match self.0 {
+				1 => EfiWarnUnknownGlyph,
+				2 => EfiWarnDeleteFailure,
+				3 => EfiWarnWriteFailure,
+				4 => EfiWarnBufferTooSmall,
+				5 => EfiWarnStaleData,
+				6 => EfiWarnFileSystem,
+				7 => EfiWarnResetRequired,
+
+				_ => UnknownWarning,
+			}
+		} else {
+			NoWarning
+		}
+	}
+
+	pub fn get_error(&self) -> EfiStatusError {
+		use EfiStatusError::*;
+
+		if self.is_error() {
+			match (self.0 << 1) >> 1 {
+				1 => EfiLoadError,
+				2 => EfiInvalidParameter,
+				3 => EfiUnsupported,
+				4 => EfiBadBufferSize,
+				5 => EfiBufferTooSmall,
+				6 => EfiNotReady,
+				7 => EfiDeviceError,
+				8 => EfiWriteProtected,
+				9 => EfiOutOfResources,
+				10 => EfiVolumeCorrupted,
+				11 => EfiVolumeFull,
+				12 => EfiNoMedia,
+				13 => EfiMediaChanged,
+				14 => EfiNotFound,
+				15 => EfiAccessDenied,
+				16 => EfiNoResponse,
+				17 => EfiNoMapping,
+				18 => EfiTimeout,
+				19 => EfiNotStarted,
+				20 => EfiAlreadyStarted,
+				21 => EfiAborted,
+				22 => EfiIcmpError,
+				23 => EfiTftpError,
+				24 => EfiProtocolError,
+				25 => EfiIncompatibleVersion,
+				26 => EfiSecurityViolation,
+				27 => EfiCrcError,
+				28 => EfiEndOfMedia,
+				31 => EfiEndOfFile,
+				32 => EfiInvalidLanguage,
+				33 => EfiCompromisedData,
+				34 => EfiIpAddressConflict,
+				35 => EfiHttpError,
+
+				_ => UnknownError,
+			}
+		} else {
+			NoError
+		}
+	}
 }
 
 impl Clone for EfiStatus {
@@ -91,4 +157,60 @@ pub enum EfiStatusEnum<T = (), E = ()> {
 	Success(T),
 	Warning(EfiStatusRaw, T),
 	Error(EfiStatusRaw, E),
+}
+
+#[non_exhaustive]
+pub enum EfiStatusWarning {
+	NoWarning,
+
+	UnknownWarning,
+
+	EfiWarnUnknownGlyph,
+	EfiWarnDeleteFailure,
+	EfiWarnWriteFailure,
+	EfiWarnBufferTooSmall,
+	EfiWarnStaleData,
+	EfiWarnFileSystem,
+	EfiWarnResetRequired,
+}
+
+#[non_exhaustive]
+pub enum EfiStatusError {
+	NoError,
+
+	UnknownError,
+
+	EfiLoadError,
+	EfiInvalidParameter,
+	EfiUnsupported,
+	EfiBadBufferSize,
+	EfiBufferTooSmall,
+	EfiNotReady,
+	EfiDeviceError,
+	EfiWriteProtected,
+	EfiOutOfResources,
+	EfiVolumeCorrupted,
+	EfiVolumeFull,
+	EfiNoMedia,
+	EfiMediaChanged,
+	EfiNotFound,
+	EfiAccessDenied,
+	EfiNoResponse,
+	EfiNoMapping,
+	EfiTimeout,
+	EfiNotStarted,
+	EfiAlreadyStarted,
+	EfiAborted,
+	EfiIcmpError,
+	EfiTftpError,
+	EfiProtocolError,
+	EfiIncompatibleVersion,
+	EfiSecurityViolation,
+	EfiCrcError,
+	EfiEndOfMedia,
+	EfiEndOfFile,
+	EfiInvalidLanguage,
+	EfiCompromisedData,
+	EfiIpAddressConflict,
+	EfiHttpError,
 }
