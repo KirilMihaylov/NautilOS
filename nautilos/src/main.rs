1f57aaefa1d0f12e844319758cf18f3a07d24d0e
Kiril Mihaylov <Kiril195@hotmail.com>
2020-04-20 00:26:51 +0300

    Corrected signature of "efi_main"

--- nautilos/src/main.rs
+++ nautilos/src/main.rs
@@ -3,7 +3,11 @@
 
 mod panic;
 
+use efi::types::EfiHandle;
+use efi::status::EfiStatus;
+use efi::system_table::EfiSystemTable;
+
 #[no_mangle]
-fn efi_main() -> usize {
+fn efi_main(_image_handle: EfiHandle, _system_table: &mut EfiSystemTable) -> EfiStatus {
 	loop {}
 }
