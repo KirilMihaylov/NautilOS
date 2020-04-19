3cccdc3298ae83d5d054e7de86203613057b62ea
Kiril Mihaylov <Kiril195@hotmail.com>
2020-04-19 14:28:58 +0300

    Added more utility functions

--- efi/src/utilities.rs
+++ efi/src/utilities.rs
@@ -1,9 +1,15 @@
-pub fn validate_string(data: &[u16]) -> Result<(), ()> {
-	use core::char::{
+use core::{
+	char::{
 		decode_utf16,
 		DecodeUtf16,
-	};
+	},
+	slice::{
+		from_raw_parts,
+		from_raw_parts_mut,
+	},
+};
 
+pub fn validate_string(data: &[u16]) -> Result<(), ()> {
 	if data.len() == 0 {
 		Err(())
 	} else {
@@ -19,6 +25,48 @@ pub fn validate_string(data: &[u16]) -> Result<(), ()> {
 		/* Doesn't have terminating-null */
 		else {
 			Err(())
+		}
+	}
+}
+
+pub unsafe fn string_length(mut string: *const u16) -> Result<usize, ()> {
+	if string.is_null() {
+		return Err(());
+	}
+
+	let mut length: usize = 0;
+	while *string != 0 {
+		string = string.offset(1);
+		length += 1;
+	}
+
+	Ok(length)
+}
+
+pub unsafe fn string_from_raw<'a>(string: *const u16) -> Result<&'a [u16], ()> {
+	if string.is_null() {
+		return Err(());
+	} else {
+		let string: &'a [u16] = from_raw_parts(string, string_length(string)?);
+
+		if decode_utf16(string.iter().map(|&x| x)).any(|c| c.is_err()) {
+			Err(())
+		} else {
+			Ok(string)
+		}
+	}
+}
+
+pub unsafe fn string_from_raw_mut<'a>(string: *mut u16) -> Result<&'a mut [u16], ()> {
+	if string.is_null() {
+		return Err(());
+	} else {
+		let string: &'a mut [u16] = from_raw_parts_mut(string, string_length(string)?);
+
+		if decode_utf16(string.iter().map(|&x| x)).any(|c| c.is_err()) {
+			Err(())
+		} else {
+			Ok(string)
 		}
 	}
 }
