fd223012582b8723eafbf51c133bdf1c64950430
Kiril Mihaylov <Kiril195@hotmail.com>
2020-04-25 13:19:30 +0300

    Added EfiObject to NVDIMMDeviceHandle conversion

--- acpi/src/devices/acpi_defined/nvdimm/device_handle.rs
+++ acpi/src/devices/acpi_defined/nvdimm/device_handle.rs
@@ -25,3 +25,44 @@ impl NVDIMMDeviceHandle {
 		((self.handle >> 16) & 4095) as u16
 	}
 }
+
+#[cfg(feature="efi_interops")]
+use efi_interops::{
+	EfiObject,
+	traits::{
+		acpi::device::NvdimmDeviceHandle,
+		conversion::FromEfiObject,
+	},
+};
+
+#[cfg(feature="efi_interops")]
+unsafe impl FromEfiObject for NVDIMMDeviceHandle {
+	fn convert(object: EfiObject) -> Option<Self> {
+		use core::{
+			mem::size_of,
+			slice::from_raw_parts_mut,
+		};
+
+		if object.len() != size_of::<Self>() {
+			None
+		} else {
+			let mut new_self: Self = Self {
+				handle: 0,
+			};
+			
+			object.copy_over(
+				unsafe {
+					from_raw_parts_mut(
+						&mut new_self as *mut Self as *mut u8,
+						size_of::<Self>()
+					)
+				}
+			);
+			
+			Some(new_self)
+		}
+	}
+}
+
+#[cfg(feature="efi_interops")]
+unsafe impl NvdimmDeviceHandle for NVDIMMDeviceHandle {}
