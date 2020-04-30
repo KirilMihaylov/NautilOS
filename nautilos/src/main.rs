07e97bfe49a6b4c20fabf57b0e593772fa683307
Kiril Mihaylov <Kiril195@hotmail.com>
2020-04-30 12:24:10 +0300

    Added comments & added panic output

--- nautilos/src/main.rs
+++ nautilos/src/main.rs
@@ -10,9 +10,19 @@ use efi::status::EfiStatus;
 use efi::system_table::EfiSystemTable;
 
 #[no_mangle]
-fn efi_main(_image_handle: EfiHandle, _system_table: &mut EfiSystemTable) -> EfiStatus {
-	if !native::features::detection::detection_mechanism_present() {
-		panic!();
+fn efi_main(_image_handle: EfiHandle, system_table: &mut EfiSystemTable) -> EfiStatus {
+	/* Verify that the system table is valid */
+	if !system_table.verify_table() {
+		return EfiStatus::error(0);
 	}
+
+	/* Set output for the panic handler */
+	panic::CON_OUT.store(system_table.con_out() as *const _ as *mut _, core::sync::atomic::Ordering::Relaxed);
+	
+	/* Requirement: Feature detection mechanism */
+	if !native::features::detection::detection_mechanism_present() {
+		panic!("No feature detection mechanism present!");
+	}
+	
 	loop {}
 }
